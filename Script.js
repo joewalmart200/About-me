c682a7bbc7db60e7104709554b5b573b0295d54a
joewalmart200 <joew91216@gmail.com>
2026-02-28 17:53:07 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,18 +1,5 @@
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({
-        behavior: "smooth"
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ 
+        behavior: 'smooth' 
     });
 }
-
-const sections = document.querySelectorAll("section");
-
-window.addEventListener("scroll", () => {
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const triggerPoint = window.innerHeight / 1.3;
-
-        if (sectionTop < triggerPoint) {
-            section.classList.add("active");
-        }
-    });
-});
